091d0dbde7fcf8b31c7c007e6579fa84d7294f70
Nick <nickkarmatskikh@gmail.com>
2018-12-24 22:35:33 +0300

    improved lab5

--- lab5/bmp.c
+++ lab5/bmp.c
@@ -1,197 +1,158 @@
 #include "bmp.h"
 
-read_status read_from_bmp(FILE *in, image *read_in){
- uint32_t width;
- uint32_t height;
+bmp_header *get_bmp_header(image *img) {
+    bmp_header *header = (bmp_header *) malloc(sizeof(bmp_header));
+    uint8_t padding = (4 - img->width * sizeof(pixel) % 4) % 4;
 
- //check type of file
- uint16_t type;
- fread(&type, 2, 1, in);
- if(type != BMP_CODE){
-   return READ_INVALID_SIGNATURE;
- }
+    //writing bmp code
+    header->type = BMP_CODE;
 
- //read file size
- uint32_t file_size;
- fread(&file_size, 4, 1, in);
+    //writing file size
+    header->file_size = (img->width * sizeof(pixel) + padding) * img->height + BMP_OFFSET;
 
- //reading width and height
- fseek(in, 12, SEEK_CUR);
- fread(&width, 4, 1, in);
- fread(&height, 4, 1, in);
+    //writing reserved bytes
+    header->reserved = 0;
 
- //set width and height
- read_in->width = width;
- read_in->height = height;
+    //writing offset
+    header->offset = BMP_OFFSET;
 
- //width's length in bytes = width * sizeof(pixel) + padding_width
- uint8_t padding_width = (4 - width * sizeof(pixel) % 4) % 4;
+    header->dib_header_size = 40;
 
- //check header
- if(((width * sizeof(pixel) + padding_width) * height  + BMP_OFFSET) != file_size){
-   return READ_INVALID_HEADER;
- }
+    //writing width and height
+    header->width = img->width;
+    header->height = img->height;
 
- //reading pixels
- uint32_t i;
- fseek(in, BMP_OFFSET, SEEK_SET);
- read_in->data = (pixel*) calloc(width * height, sizeof(pixel));
- uint32_t skiped_padding = 0;
- for(i = 0; i < height * (width + 1); i++){
-   if((i + 1) % (width + 1) == 0){
-     fseek(in, padding_width, SEEK_CUR);
-     skiped_padding++;
-   }
-   else{
-     pixel *px = read_in->data + i - skiped_padding;
-     fread(px, sizeof(pixel), 1, in);
-   }
- }
- return READ_OK;
+    //writing planes
+    header->planes = 1;
+
+    //writing bits per pixel
+    header->bits_pixel = 24;
+
+    header->compression = 0;
+    header->image_size_bytes = (img->width * sizeof(pixel) + padding) * img->height;
+    header->x_ppm = 0xb12;
+    header->y_ppm = 0xb12;
+    header->num_colors = 0;
+    header->important_colors = 0;
+    return header;
 }
 
-BMPHeader * get_bmp_header(image *img){
-  BMPHeader *header = (BMPHeader*) malloc(sizeof(BMPHeader));
-  uint8_t padding = (4 - img->width * sizeof(pixel) % 4) % 4;
+read_status read_from_bmp(FILE *in, image *read_in) {
+    uint32_t width;
+    uint32_t height;
 
-  //writing bmp code
-  header->type = BMP_CODE;
+    //check type of file
+    uint16_t type;
+    fread(&type, 2, 1, in);
+    if (type != BMP_CODE) {
+        return READ_INVALID_SIGNATURE;
+    }
 
-  //writing file size
-  header->file_size = (img->width * sizeof(pixel) + padding) * img->height + BMP_OFFSET;
+    //read file size
+    uint32_t file_size;
+    fread(&file_size, 4, 1, in);
 
-  //writing reserved bytes
-  header->reserved = 0;
+    //reading width and height
+    fseek(in, 12, SEEK_CUR);
+    fread(&width, 4, 1, in);
+    fread(&height, 4, 1, in);
 
-  //writing offset
-  header->offset = BMP_OFFSET;
+    //set width and height
+    read_in->width = width;
+    read_in->height = height;
 
-  header->dib_header_size = 40;
+    //width's length in bytes = width * sizeof(pixel) + padding_width
+    uint8_t padding_width = (4 - width * sizeof(pixel) % 4) % 4;
 
-  //writing width and height
-  header->width = img->width;
-  header->height = img->height;
-
-  //writing planes
-  header->planes = 1;
-
-  //writing bits per pixel
-  header->bits_pixel = 24;
-
-  header->compression = 0;
-  header->image_size_bytes = (img->width * sizeof(pixel) + padding) * img->height;
-  header->x_ppm = 0xb12;
-  header->y_ppm = 0xb12;
-  header->num_colors = 0;
-  header->important_colors = 0;
-  return header;
+    int file_descriptor = fileno(in);
+    read_in->data = mmap(NULL, (width * sizeof(pixel) + padding_width) * height, PROT_READ | PROT_WRITE,
+            MAP_SHARED, file_descriptor, 0);
+    read_in->data = (pixel*) ((uint8_t*) read_in->data + BMP_OFFSET);
+    return READ_OK;
 }
 
-write_status write_to_bmp(FILE *out, image *img){
-  //writing header
-  BMPHeader *header = get_bmp_header(img);
-  fwrite(header, sizeof(BMPHeader), 1, out);
+write_status write_to_bmp(FILE *out, image *img) {
+    //writing header
+    fseek(out, 0, SEEK_SET);
+    bmp_header *header = get_bmp_header(img);
+    fwrite(header, sizeof(bmp_header), 1, out);
 
-  uint8_t padding = (4 - img->width * sizeof(pixel) % 4) % 4;
-  //writing pixels
-  uint64_t i;
-  uint32_t zero_padding = 0;
-  for(i = 0; i < img->height * (img->width + 1); i++){
-    pixel *px = img->data + i;
-    px = (pixel*) (((uint8_t*) px) - zero_padding * (3 - padding));
-    if((i + 1) % (img->width + 1) == 0){
-      fwrite(px, 1, padding, out);
-      zero_padding++;
+    uint8_t padding = (4 - img->width * sizeof(pixel) % 4) % 4;
+    //writing pixels
+    uint64_t i;
+    uint32_t zero_padding = 0;
+    for (i = 0; i < img->height * (img->width + 1); i++) {
+        pixel *px = img->data + i;
+        px = (pixel *) (((uint8_t *) px) - zero_padding * (3 - padding));
+        if ((i + 1) % (img->width + 1) == 0) {
+            fwrite(px, 1, padding, out);
+            zero_padding++;
+        } else {
+            fwrite(px, 1, 3, out);
+        }
     }
-    else{
-      fwrite(px, 1, 3, out);
-    }
-  }
-  return WRITE_OK;
+    return WRITE_OK;
 }
 
-image* rotate(image *source_image){
-  image* new_image = (image*) malloc(sizeof(image));
-  new_image->width = source_image->height;
-  new_image->height = source_image->width;
+image *rotate(image *source_image) {
+    image *new_image = malloc(sizeof(image));
+    new_image->height = source_image->width;
+    new_image->width = source_image->height;
 
-  uint8_t new_padding = (4 - new_image->width * sizeof(pixel) % 4) % 4;
-  new_image->data = (pixel*) malloc((new_image->width * sizeof(pixel) + new_padding) * new_image->height);
+    uint8_t old_padding = (4 - source_image->width * sizeof(pixel) % 4) % 4;
+    uint8_t new_padding = (4 - new_image->width * sizeof(pixel) % 4) % 4;
+    new_image->data = mmap(NULL, (new_image->width * sizeof(pixel) + new_padding) * new_image->height,
+            PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, 0, 0);
 
-  uint32_t row;
-  uint32_t column;
-  for(row = 0; row < new_image->height; row++){
-    for (column = 0; column < new_image->width; column++) {
-      pixel *px = source_image->data + source_image->width * (column + 1) - row - 1;
-      pixel *new_px = new_image->data + column + row * new_image->width;
-      new_px = (pixel*) ((uint8_t*) new_px + row * new_padding);
-      memcpy(new_px, px, sizeof(pixel));
+    uint32_t row;
+    uint32_t column;
+    for (row = 0; row < new_image->height; row++) {
+        pixel source_px[new_image->width];
+        for (column = 0; column < new_image->width; column++) {
+            pixel *px = (void*)(source_image->data + source_image->width * (column + 1) - row - 1) + old_padding * column;
+            source_px[column] = *px;
+        }
+        void *dest = (void*)new_image->data + (new_image->width * sizeof(pixel) + new_padding) * row;
+        memcpy(dest, source_px, new_image->width * sizeof(pixel));
     }
-     uint8_t *zero_padding = ((uint8_t*)new_image->data) + new_image->width * sizeof(pixel) * (row + 1) + row * new_padding;
-     uint8_t i;
-     for(i = 0; i <= new_padding - 1; i++)
-       *(zero_padding + i) = 0;
-  }
-  return new_image;
+    //munmap(source_image->data, (source_image->width + old_padding) * source_image->height);
+    return new_image;
 }
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[]) {
 
-  struct option long_options[] = {
-    {"file", required_argument, NULL, 0},
-    {"rotation", optional_argument, NULL, 1},
-    {NULL, 0, NULL, 0}
-  };
-  int option_index;
-  char *file_name = NULL;
-  int angle = 90;
+    struct option long_options[] = {
+            {"file",     required_argument, NULL, 0},
+            {"rotation", optional_argument, NULL, 1},
+            {NULL, 0, NULL, 0}
+    };
+    int option_index;
+    char *file_name;
+    int angle = 90;
 
-  while(1){
-    int flag = getopt_long_only(argc, argv, "", long_options, &option_index);
+    while (1) {
+        int flag = getopt_long_only(argc, argv, "", long_options, &option_index);
+        if (flag == -1)
+            break;
 
-    if(flag == -1)
-      break;
-
-    switch(flag){
-      case 0:
-        file_name = optarg;
-        break;
-      case 1:
-        angle = atoi(optarg);
-        break;
-      default:
-        printf("%s\n", "Wrong arguments");
+        switch (flag) {
+            case 0:
+                file_name = optarg;
+                break;
+            case 1:
+                angle = atoi(optarg);
+                break;
+            default:
+                printf("%s\n", "Wrong arguments");
+        }
     }
-  }
-  FILE *file_image = fopen(file_name, "rb");
-  if(!file_image){
-    puts("File does not exist");
-    exit(1);
-  }
 
-  image *img = (image*) malloc(sizeof(image));
-  read_status rd_status = read_from_bmp(file_image, img);
-  fclose(file_image);
-  if(rd_status == READ_INVALID_SIGNATURE){
-    puts("Invalid file's signature");
-    exit(1);
-  }
-  else if(rd_status == READ_INVALID_HEADER){
-    puts("Invalid bmp header");
-    exit(1);
-  }
+    FILE *test = fopen(file_name, "r+b");
+    image img;
+    read_status rd_status = read_from_bmp(test, &img);
 
-  uint16_t i;
-  for(i = 0; i < ((angle / 90) % 4); i++){
-    FILE *file_image = fopen(file_name, "rb");
-    image *img = (image*) malloc(sizeof(image));
-    read_from_bmp(file_image, img);
-    fclose(file_image);
-    image *new_one = rotate(img);
-
-    FILE *new_img = fopen(file_name, "wb");
-    write_status wr_status = write_to_bmp(new_img, new_one);
-    fclose(new_img);
-  }
-  return 0;
+    image *new_image = rotate(&img);
+    write_to_bmp(test, new_image);
+    fclose(test);
+    return 0;
 }
--- lab5/bmp.h
+++ lab5/bmp.h
@@ -1,53 +1,54 @@
 #include <stdio.h>
 #include <stdint.h>
-#include <malloc.h>
 #include <string.h>
 #include <stdlib.h>
+#include <sys/mman.h>
+#include <errno.h>
 #include <getopt.h>
+
 #define BMP_CODE 0x4d42
 #define BMP_OFFSET 54
 
-typedef struct __attribute__((packed)){
-  uint16_t type;
-  uint32_t file_size;
-  uint32_t reserved;
-  uint32_t offset;
-  uint32_t dib_header_size;
+typedef struct __attribute__((packed)) {
+    uint16_t type;
+    uint32_t file_size;
+    uint32_t reserved;
+    uint32_t offset;
+    uint32_t dib_header_size;
 
-  uint32_t width;
-  uint32_t height;
-  uint16_t planes;
-  uint16_t bits_pixel;
-  uint32_t compression;
-  uint32_t image_size_bytes;
-  uint32_t x_ppm;
-  uint32_t y_ppm;
-  uint32_t num_colors;
-  uint32_t important_colors;
-} BMPHeader;
+    uint32_t width;
+    uint32_t height;
+    uint16_t planes;
+    uint16_t bits_pixel;
+    uint32_t compression;
+    uint32_t image_size_bytes;
+    uint32_t x_ppm;
+    uint32_t y_ppm;
+    uint32_t num_colors;
+    uint32_t important_colors;
+} bmp_header;
 
-typedef struct{
-  uint8_t blue, green, red;
+typedef struct {
+    uint8_t blue, green, red;
 } pixel;
 
-typedef struct{
-  uint32_t width, height;
-  pixel *data;
+typedef struct {
+    uint32_t width, height;
+    pixel *data;
 } image;
 
-typedef enum read_status{
-  READ_OK = 0,
-  READ_INVALID_SIGNATURE,
-  READ_INVALID_HEADER
+typedef enum read_status {
+    READ_OK = 0,
+    READ_INVALID_SIGNATURE,
 } read_status;
 
-typedef enum write_status{
-  WRITE_OK = 0,
-  WRITE_ERROR
+typedef enum write_status {
+    WRITE_OK = 0,
+    WRITE_ERROR
 } write_status;
 
 read_status read_from_bmp(FILE *in, image *read_in);
 
-image* rotate(image *source_image);
-
 write_status write_to_bmp(FILE *out, image *img);
+
+image *rotate(image *source_image);
