62b7655f0d060dce618bdd70d73bb1f5cfb2a49f
Nick <nickkarmatskikh@gmail.com>
2019-01-18 21:03:38 +0300

    added sse sepia filter

--- lab7/sepia_filter.c
+++ lab7/sepia_filter.c
@@ -21,14 +21,55 @@ static void sepia_one(pixel* px ) {
         px->blue = sat(old.red * c[2][0] + old.green * c[2][1] + old.blue * c[2][2]);
 }
 
-void sepia_chunk(pixel* data){
-  const float coeffs[3][4] = {
-          {.393f, .349f, .272f, .393f},
-          {.769f, .686f, .543f, .769f},
-          {.189f, .168f, .131f, .272f}
+static void sepia_chunk(pixel* data){
+  const float coeffs[3][12] = {
+          {.131f, .168f, .189f, .131f, .168f, .189f, .131f, .168f, .189f, .131f, .168f, .189f},
+          {.543f, .686f, .769f, .543f, .686f, .769f, .543f, .686f, .769f, .543f, .686f, .769f},
+          {.272f, .349f, .393f, .272f, .349f, .393f, .272f, .349f, .393f, .272f, .349f, .393f}
   };
-  set_coeffs(coeffs[0], coeffs[1], coeffs[2]);
+  uint8_t results[12];
 
+  int temp = 3;
+  for(int i = 0; i < 3; i++){
+    pixel *px_1 = data + i;
+    pixel *px_2 = data + i + 1;
+
+    float blue[4];
+    float green[4];
+    float red[4];
+    float result[4];
+
+    for(int j = 0; j < temp; j++){
+      blue[j] = px_1->blue;
+      green[j] = px_1->green;
+      red[j] = px_1->red;
+    }
+
+    for(int j = temp; j < 4; j++){
+      blue[j] = px_2->blue;
+      green[j] = px_2->green;
+      red[j] = px_2->red;
+    }
+
+    float coeffs_1[4];
+    float coeffs_2[4];
+    float coeffs_3[4];
+
+    for(int j = 0; j < 4; j++){
+      coeffs_1[j] = coeffs[0][j + 4 * i];
+      coeffs_2[j] = coeffs[1][j + 4 * i];
+      coeffs_3[j] = coeffs[2][j + 4 * i];
+    }
+
+    set_coeffs(coeffs_1, coeffs_2, coeffs_3);
+    sepia_one_sse(blue, green, red, result);
+
+    for(int j = 0; j < 4; j++){
+      results[4 * i + j] = sat(result[j]);
+    }
+    temp--;
+  }
+  memcpy(data, results, 12);
 }
 
 void sepia_filter_sse(image *img){
